ed3f5113e8403a4a41cbe2d396c2ca2d250a41a7
ramoops <seitakutaken@gmail.com>
2024-07-27 23:23:34 +0300

    Initial commit

---
+++ config.h
@@ -0,0 +1,21 @@
+// Copyright 2023 linlin012 (@linlin012)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#undef PERMISSIVE_HOLD
+#undef PREVENT_STUCK_MODIFIERS
+
+#define TAPPING_TERM 200
+
+#define CAPS_LOCK_INDEX 28
+#define WIN_MOD_INDEX 16
+#define MAC_MOD_INDEX 17
+#define AW20216S_SDB_PIN A4
+#define TAPPING_TERM_PER_KEY
+
+#define AW20216S_CS_PIN_1 B12
+#define AW20216S_EN_PIN_1 A4
+
+#define ENABLE_RGB_MATRIX_PIXEL_FRACTAL
+#define ENABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
